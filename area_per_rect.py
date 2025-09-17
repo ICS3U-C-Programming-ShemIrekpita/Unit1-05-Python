54ce517287e7290420853c4119bb8a02639b7187
Shem Irekpita <shem.irekpita@stu.ocsb.ca>
2025-09-17 18:19:37 +0000

    this program calculates the area and perimiter of a rectangle

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Shem
+# 9/17/2025
+# this program calculates the area and perimiter of a rectangle
+
+
+def main():
+    print("For a rectangle with length 10cm and width 20cm:")
+    print()
+    print("The area is: {} cm^2".format(10 * 20))
+    print("The perimeter is: {} cm".format(2 * (10 + 20)))
+
+
+if __name__ == "__main__":
+    main()
